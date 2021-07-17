f394f0ce28f2a0345e3c4b5a40d44285bb98188f
PanavSethi <panav.sethi.delhi@gmail.com>
2021-07-17 11:22:01 +0530

    build: styles changed and create post started

--- src/components/posts/CreatePost.tsx
+++ src/components/posts/CreatePost.tsx
@@ -2,44 +2,26 @@ import React from 'react'
 import user from '../../assets/images/icons/user-profile.svg'
 import photo from '../../assets/images/icons/photo.svg'
 import video from '../../assets/images/icons/video.svg'
+import { useState } from 'react'
 
 const CreatePost: React.FC = () => {
+  const [postOptionsShow, setPostOptionsShow] = useState<boolean>(false)
   return (
     <div className="feed__center__create-post">
-      <div className="feed__center__create-post__text-area">
+      <div
+        onClick={() => setPostOptionsShow(true)}
+        className="feed__center__create-post__text-area"
+      >
         <img
           className="feed__center__create-post__text-area__image"
           src={user}
           alt=""
         ></img>
         <textarea
+          disabled
           className="input-component--post"
           placeholder="Let your ideas flow..."
         ></textarea>
-      </div>
-      <div className="feed__center__create-post__actions">
-        <a href="/" className="feed__center__create-post__actions__link">
-          <img
-            src={photo}
-            alt=""
-            className="feed__center__create-post__actions__link__img"
-          ></img>
-          Photo
-        </a>
-        <a href="/" className="feed__center__create-post__actions__link">
-          <img
-            src={video}
-            alt=""
-            className="feed__center__create-post__actions__link__img"
-          ></img>
-          Video
-        </a>
-        <a
-          href="/"
-          className="feed__center__create-post__actions__button button button--purple"
-        >
-          Post
-        </a>
       </div>
     </div>
   )
